f11545933d880d8a254a3260f514d92bc3f21713
Eric Dennison <ericd@netdenizen.com>
2022-12-04 14:40:20 -0500

    Regex implementation is a little clearer

--- dec04.py
+++ dec04.py
@@ -1,15 +1,15 @@
 # Advent of code 04
 from aocutils import *
+from re import findall
 
 # Sample:  2 and 518
 # Actual:  4 and 909
 
 def dec04(fname):
-    lines = flistofstrings(fname)
-    pairs = [[[int(z) for z in y.split('-')] for y in x] for x in 
-                [x.split(',') for x in lines]]
-    print(f"part 1: {sum([rangecontainsrange(*a[0],*a[1]) for a in pairs])}")
-    print(f"part 2: {sum([rangeoverlapsrange(*a[0],*a[1]) for a in pairs])}")
+    s = "(\d+)-(\d+),(\d+)-(\d+)"
+    data = [[int(y) for y in findall(s,x)[0]] for x in flistofstrings(fname)]
+    print(f"part 1: {sum([rangecontainsrange(*a) for a in data])}")
+    print(f"part 2: {sum([rangeoverlapsrange(*a) for a in data])}")
 
 print("Sample")
 dec04("dec04s.txt")
