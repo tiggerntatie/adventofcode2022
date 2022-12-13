4e72ddb560d588e818afc1d4c710387a29ab5827
Eric Dennison <ericd@netdenizen.com>
2022-12-12 19:56:25 -0500

    Updated from Brython Server: 12/12/2022 7:56:25 PM

--- dec12.py
+++ dec12.py
@@ -1,1 +1,29 @@
+# Advent of code 12
+from aocutils import *
+from re import compile
+# Sample:  31 and 
+# Actual:   and 
 
+def dec12(fname):
+    [Monkey(*m, 1) for m in groupedstringlists(flistofstrings(fname))]
+    for _ in range(20):
+        for mid in range(len(Monkey.list)):
+            m = Monkey.monkeyfromid(mid)
+            m.turn()
+    #print([m.inspectcount for m in Monkey.list.values()])
+    top2 = sorted([m.inspectcount for m in Monkey.list.values()])[-2:]
+    print(f"part 1: {top2[0]*top2[1]}")
+    Monkey.clearmonkies()
+    [Monkey(*m, 2) for m in groupedstringlists(flistofstrings(fname))]
+    for _ in range(10000):
+        for mid in range(len(Monkey.list)):
+            m = Monkey.monkeyfromid(mid)
+            m.turn()
+    #print([m.inspectcount for m in Monkey.list.values()])
+    top2 = sorted([m.inspectcount for m in Monkey.list.values()])[-2:]
+    print(f"part 2: {top2[0]*top2[1]}")
+
+print("Sample")
+dec12("dec12s.txt")
+#print("Actual")
+#dec12("dec12.txt")
