b4013a9c7a8cd7d4b703042d25fec1cd73277845
Eric Dennison <ericd@netdenizen.com>
2022-12-12 20:33:36 -0500

    Updated from Brython Server: 12/12/2022 8:33:36 PM

--- dec12.py
+++ dec12.py
@@ -4,26 +4,23 @@ from re import compile
 # Sample:  31 and 
 # Actual:   and 
 
+nodes = {}
+unvisited = []
+
+def sortunvisited():
+    unvisited.sort(key = lambda x: nodes[x])
+
 def dec12(fname):
-    [Monkey(*m, 1) for m in groupedstringlists(flistofstrings(fname))]
-    for _ in range(20):
-        for mid in range(len(Monkey.list)):
-            m = Monkey.monkeyfromid(mid)
-            m.turn()
-    #print([m.inspectcount for m in Monkey.list.values()])
-    top2 = sorted([m.inspectcount for m in Monkey.list.values()])[-2:]
-    print(f"part 1: {top2[0]*top2[1]}")
-    Monkey.clearmonkies()
-    [Monkey(*m, 2) for m in groupedstringlists(flistofstrings(fname))]
-    for _ in range(10000):
-        for mid in range(len(Monkey.list)):
-            m = Monkey.monkeyfromid(mid)
-            m.turn()
-    #print([m.inspectcount for m in Monkey.list.values()])
-    top2 = sorted([m.inspectcount for m in Monkey.list.values()])[-2:]
+    map = flistofstrings(fname)
+    for x in range(len(map[0])):
+        for y in range(len(map)):
+            node = (x,y)
+            nodes[node] = 1000000    # infinite distance
+            unvisited.append(node)
+    
     print(f"part 2: {top2[0]*top2[1]}")
 
 print("Sample")
 dec12("dec12s.txt")
-#print("Actual")
+print("Actual")
 #dec12("dec12.txt")
