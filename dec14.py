4cbe105f3a02cd3543d0b652f82348b94bb74228
Eric Dennison <ericd@netdenizen.com>
2022-12-14 17:20:24 -0500

    Updated from Brython Server: 12/14/2022 5:20:23 PM

--- dec14.py
+++ dec14.py
@@ -9,7 +9,28 @@ def rangeupdown(start, end):
     step = 1 if end > start else -1
     return range(start, end+step, step)
 
-
+# run a sand grain down
+# sld is the slice map
+# pos is the grain position (x,y) tuple
+# bottom is the lowest piece of rock y coord
+# returns True if grain can't move further
+# returns False if grain falls below the bottom
+def sandstep(sld, pos, bottom):
+    if pos[1] == bottom:
+        # freefall now
+        return False
+    if (pos[0],pos[1]+1) in sld:
+        if (pos[0]-1,pos[1]+1) in sld:
+            if (pos[0]+1,pos[1]+1) in sld:
+                # can't move
+                sld[pos] = 'o'
+                return True
+            else:
+                return sandstep(sld, (pos[0]+1,pos[1]+1), bottom)
+        else:
+            return sandstep(sld, (pos[0]-1,pos[1]+1), bottom)
+    else:
+        return sandstep(sld, (pos[0],pos[1]+1), bottom)
 
 def dec14(fname):
     sld = {}
@@ -22,7 +43,10 @@ def dec14(fname):
                     sld[(x,y)] = '#'
                     if y > lowestrock:
                         lowestrock = y
-    #print(f"part 1: {len(visitedk1)}")
+    grains = 0
+    while sandstep(sld, (500,0), lowestrock):
+        grains += 1
+    print(f"part 1: {grains}")
     #print(f"part 2: {len(visitedk9)}")
  
 print("Sample")
