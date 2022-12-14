0ec22620f23bd0f7011b0efbb7c65a3288b155bc
Eric Dennison <ericd@netdenizen.com>
2022-12-14 16:48:18 -0500

    Updated from Brython Server: 12/14/2022 4:48:09 PM

--- dec14.py
+++ dec14.py
@@ -1,1 +1,20 @@
+# Advent of code 14
+from aocutils import *
+#from re import compile
+# Sample:  24 and 
+# Actual:   and 
 
+
+def dec14(fname):
+    sld = {}
+    for path in flistofstrings(fname):
+        print("path")
+        for step in path.split(' -> '):
+            print(step)
+    #print(f"part 1: {len(visitedk1)}")
+    #print(f"part 2: {len(visitedk9)}")
+ 
+print("Sample")
+dec14("dec14s.txt")
+#print("Actual")
+#dec14("dec14.txt")
