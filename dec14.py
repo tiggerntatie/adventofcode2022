dbd9ab2f79ea54082a91b2b6e48e25efcdf34fd2
Eric Dennison <ericd@netdenizen.com>
2022-12-14 17:06:38 -0500

    Updated from Brython Server: 12/14/2022 5:06:38 PM

--- dec14.py
+++ dec14.py
@@ -9,16 +9,19 @@ def rangeupdown(start, end):
     step = 1 if end > start else -1
     return range(start, end+step, step)
 
+
+
 def dec14(fname):
     sld = {}
+    lowestrock = 0
     for path in flistofstrings(fname):
         steps = [[int(y) for y in x.split(',')] for x in path.split(' -> ')]
         for i in range(len(steps)-1):
             for x in rangeupdown(steps[i][0],steps[i+1][0]):
-                for y in rangeupdown(steps[i][1],steps[i+1][1])
+                for y in rangeupdown(steps[i][1],steps[i+1][1]):
                     sld[(x,y)] = '#'
-                    print(x,y)
-        print(steps)
+                    if y > lowestrock:
+                        lowestrock = y
     #print(f"part 1: {len(visitedk1)}")
     #print(f"part 2: {len(visitedk9)}")
  
