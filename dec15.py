79d9dea9b6fde24a1f0da790ed681f299f7a975d
Eric Dennison <ericd@netdenizen.com>
2022-12-15 08:48:32 -0500

    Updated from Brython Server: 12/15/2022 8:48:32 AM

--- dec15.py
+++ dec15.py
@@ -1,16 +1,18 @@
 # Advent of code 15
+from re import compile
 from aocutils import *
 # Sample:   and 
 # Actual:   and  
 
 
 def dec15(fname):
-    for _ in flistofstrings(fname):
-        pass
-    print(f"part 1: {grains}")
-    print(f"part 2: {grains}")
+    ins = compile(".*([\d\-]+).*([\d\-]+).*([\d\-]+).*([\d\-]+)")
+    ml = [[int.m.group(i) for i in range(1,5)] for m in [ins.search(s) for s in flistofstrings(fname)]
+    print(ml)
+    #print(f"part 1: {grains}")
+    #print(f"part 2: {grains}")
  
 print("Sample")
 dec15("dec15s.txt")
-print("Actual")
-dec15("dec15.txt")
+#print("Actual")
+#dec15("dec15.txt")
