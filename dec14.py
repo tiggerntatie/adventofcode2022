844410dbe60dbb800d576098be9d075567d9a92b
Eric Dennison <ericd@netdenizen.com>
2022-12-14 19:55:44 -0500

    Updated from Brython Server: 12/14/2022 7:55:43 PM

--- dec14.py
+++ dec14.py
@@ -15,25 +15,32 @@ def rangeupdown(start, end):
 # bottom is the lowest piece of rock y coord
 # returns True if grain can't move further
 # returns False if grain falls below the bottom
-def sandstep(sld, pos, bottom):
-    if pos[1] == bottom:
-        # freefall now
+def sandstep(sld, pos, bottom, newgrainlist, part):
+    if (part == 1 and pos[1] == bottom) or (part == 2 and (500,0) in sld):
+        # freefall (1) or top is full (2)
         return False
+    if pos[1] == bottom+1:
+        # on floor.. can't move
+        sld[pos] = '0'
+        return True
     if (pos[0],pos[1]+1) in sld:
         if (pos[0]-1,pos[1]+1) in sld:
             if (pos[0]+1,pos[1]+1) in sld:
                 # can't move
                 sld[pos] = 'o'
+                if part == 1:
+                    newgrainlist.append(pos)
                 return True
             else:
-                return sandstep(sld, (pos[0]+1,pos[1]+1), bottom)
+                return sandstep(sld, (pos[0]+1,pos[1]+1), bottom, newgrainlist, part)
         else:
-            return sandstep(sld, (pos[0]-1,pos[1]+1), bottom)
+            return sandstep(sld, (pos[0]-1,pos[1]+1), bottom, newgrainlist, part)
     else:
-        return sandstep(sld, (pos[0],pos[1]+1), bottom)
+        return sandstep(sld, (pos[0],pos[1]+1), bottom, newgrainlist, part)
 
 def dec14(fname):
     sld = {}
+    newgrains = []
     lowestrock = 0
     for path in flistofstrings(fname):
         steps = [[int(y) for y in x.split(',')] for x in path.split(' -> ')]
@@ -44,10 +51,16 @@ def dec14(fname):
                     if y > lowestrock:
                         lowestrock = y
     grains = 0
-    while sandstep(sld, (500,0), lowestrock):
+    while sandstep(sld, (500,0), lowestrock, newgrains, 1):
         grains += 1
     print(f"part 1: {grains}")
-    #print(f"part 2: {len(visitedk9)}")
+    # clear out
+    for g in newgrains:
+        sld.pop(g)
+    grains = 0
+    while sandstep(sld, (500,0), lowestrock, newgrains, 2):
+        grains += 1
+    print(f"part 2: {grains}")
  
 print("Sample")
 dec14("dec14s.txt")
