dc58748cc12c354d0d25fcf8bf328b6fa1742e1a
Eric Dennison <ericd@netdenizen.com>
2022-12-15 09:56:20 -0500

    Updated from Brython Server: 12/15/2022 9:56:19 AM

--- dec15.py
+++ dec15.py
@@ -14,7 +14,7 @@ def boundsdist(yrow, dist, x, y):
         return (0,0) # nothing on this row is excluded
     return (x-deltax, x+deltax)
 
-def excludedcount(ml, row):
+def oldexcludedcount(ml, row):
     ranges = [boundsdist(row, l[4], l[0], l[1]) for l in ml]
     exc = set()
     for r in ranges:
@@ -27,6 +27,44 @@ def excludedcount(ml, row):
             exc.remove(l[2])
     return len(exc)
 
+def aoverlapb(a1, a2, b1, b2):
+    if a1 >= b1 and a2 <= b2: # a inside b
+        return (b1, b2)
+    if b1 >= a1 and b2 <= a2: # b inside a
+        return (a1, b2)
+    if a1 < b1 and a2 <= b2: # a then overlap b
+        return (a1, b2)
+    if b1 < a1 and b2 <= a2: # b then overlap a
+        return (b1, a2)
+    return False
+
+def excludedcount(ml, row):
+    ranges = [boundsdist(row, l[4], l[0], l[1]) for l in ml]
+    # remove null ranges
+    ranges = filter(lambda x: x[0] != x[1], ranges)
+    done = False
+    while not done:
+        restart = False
+        for i in ranges(len(ranges)-1):
+            for j in ranges(1, len(ranges)):
+                if crange := aoverlapb(*ranges[i], *ranges[j]):
+                    ranges[i] = crange
+                    del(ranges[j])
+                    restart = True
+                    break
+                elif i == len(ranges)-2 and j == len(ranges)-1:
+                    done = True
+            if restart:
+                break
+    count = 0
+    for r in ranges:
+        count += r[1]-r[0]
+        for l in ml:    # remove any beacons in this range
+            if l[3] == row and l[2] >= r[0] and l[2] <= r[1]:
+                count -= 1
+    return count
+
+
 def dec15(fname, row):
     ins = compile(".*=([\d\-]+).*=([\d\-]+).*=([\d\-]+).*=([\d\-]+)")
     ml = [[int(m.group(i)) for i in range(1,5)] for m in [ins.search(s) for s in flistofstrings(fname)]]
@@ -38,4 +76,4 @@ def dec15(fname, row):
 print("Sample")
 dec15("dec15s.txt", 10)
 print("Actual")
-dec15("dec15.txt", 2000000)
+#dec15("dec15.txt", 2000000)
