91b1117ca73807189e7f96d4448aff293e6728bc
Eric Dennison <ericd@netdenizen.com>
2022-12-15 09:03:42 -0500

    Updated from Brython Server: 12/15/2022 9:03:41 AM

--- dec15.py
+++ dec15.py
@@ -1,13 +1,27 @@
 # Advent of code 15
 from re import compile
 from aocutils import *
-# Sample:   and 
-# Actual:   and  
+# Sample: row 10: 26  and 
+# Actual: row 2000000:  and  
 
+def mdist(x1,y1,x2,y2):
+    return abs(x1-x2)+abs(y1-y2)
+
+# start and end x-coordinates for y-row positions less than distance to x,y
+def boundsdist(yrow, dist, x, y):
+    deltax = dist-abs(y-yrow)
+    if deltax <= 0:
+        return (0,0) # nothing on this row is excluded
+    return (x-deltax+1, x+deltax-1)
+
+def excludedcount(ml, row):
+    #ranges = [boundsdist(row, )]
+    pass
 
 def dec15(fname):
     ins = compile(".*=([\d\-]+).*=([\d\-]+).*=([\d\-]+).*=([\d\-]+)")
     ml = [[int(m.group(i)) for i in range(1,5)] for m in [ins.search(s) for s in flistofstrings(fname)]]
+    ml = [x.extend(mdist(*x)) for x in ml]
     print(ml)
     #print(f"part 1: {grains}")
     #print(f"part 2: {grains}")
