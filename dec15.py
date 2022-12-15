fb88b79dec8532b883cb40d327139c1bff5ba902
Eric Dennison <ericd@netdenizen.com>
2022-12-15 08:39:16 -0500

    Create dec15.py

---
+++ dec15.py
@@ -0,0 +1,16 @@
+# Advent of code 15
+from aocutils import *
+# Sample:   and 
+# Actual:   and  
+
+
+def dec15(fname):
+    for _ in flistofstrings(fname):
+        pass
+    print(f"part 1: {grains}")
+    print(f"part 2: {grains}")
+ 
+print("Sample")
+dec15("dec15s.txt")
+print("Actual")
+dec15("dec15.txt")
