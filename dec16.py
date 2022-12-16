01d382eb1b939c6fca305e1b8ce3bd6245c80dc9
Eric Dennison <ericd@netdenizen.com>
2022-12-16 12:48:50 -0500

    Updated from Brython Server: 12/16/2022 12:48:49 PM

--- dec16.py
+++ dec16.py
@@ -5,16 +5,20 @@ from aocutils import *
 # Sample: 1651 and 
 # Actual: and 
 
+def bestvalveflow(vd, startvalve, timeleft):
+    if not timeleft:
+        return 0
+    if 
      
 # example input line
 # Valve BB has flow rate=13; tunnels lead to valves CC, AA
 def dec16(fname):
     vd = {}
     valves = compile("Valve (\S+) has flow rate=(\d+); tunnels? leads? to valves? (.+)")
-    #ml = [[int(m.group(i)) for i in range(1,5)] for m in [valvessearch(s) for s in flistofstrings(fname)]]
+    # flow rate, open/closed, connected valve list
     for v in [valves.search(s) for s in flistofstrings(fname)]:
-        vd[v.group(1)] = [int(v.group(2)), v.group(3).split(', ')]
-    print(vd)
+        vd[v.group(1)] = [int(v.group(2)), 0, v.group(3).split(', ')]
+    flowrate = bestvalveflow(vd, 'AA', 30)
     #print(f"part 1: {count}")
 
     #print(f"part 2: {tfreq}")
