932c63d38da4917dedba7a1f3c66b0163b134d58
Eric Dennison <ericd@netdenizen.com>
2022-12-16 12:40:22 -0500

    Updated from Brython Server: 12/16/2022 12:40:22 PM

--- dec16.py
+++ dec16.py
@@ -10,7 +10,7 @@ from aocutils import *
 # Valve BB has flow rate=13; tunnels lead to valves CC, AA
 def dec16(fname):
     vd = {}
-    valves = compile("Valve ([\S]+) has flow rate=(\d+); tunnels? leads? to valves? (.+)")
+    valves = compile("Valve (\S+) has flow rate=(\d+); tunnels? leads? to valves? (.+)")
     #ml = [[int(m.group(i)) for i in range(1,5)] for m in [valvessearch(s) for s in flistofstrings(fname)]]
     for v in [valves.search(s) for s in flistofstrings(fname)]:
         vd[v.group(1)] = [int(v.group(2)), v.group(3).split(',')]
