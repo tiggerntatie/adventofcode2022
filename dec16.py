4598fe68bb7d938acb0e07bbb8804aa3eca4c5aa
Eric Dennison <ericd@netdenizen.com>
2022-12-16 13:43:31 -0500

    Updated from Brython Server: 12/16/2022 1:43:30 PM

--- dec16.py
+++ dec16.py
@@ -6,28 +6,26 @@ from aocutils import *
 # Actual: and 
 
 valvecount = 0
-valveson = 0
 #  best valve flow
-def bvf(vd, startvalve, timeleft):
+def bvf(vd, startvalve, timeleft, valveson):
     global valveson
     if valveson == valvecount:
-        return 0
+        return (0, valveson)
     if timeleft <= 1:
         # no time left to add more flow
-        return 0
+        return (0, valveson)
     v = vd[startvalve]
     if v[1] or v[0] == 0:    # valve already open, or 0 flow, get times to other nodes
         t1 = [bvf(vd, xx, timeleft-1) for xx in v[2]]
-        return max(t1)        
+        return (max(t1), valveson)        
     else:
         # assume opening valve: remaining flow for this valve plus the rest
         if timeleft == 2:
-            return v[0]
+            return (v[0], valveson)
         else:
-            valveson += 1
             v[1] = 1
             t2 = [v[0]*(timeleft-1) + bvf(vd, xx, timeleft-2) for xx in v[2]]
-            return max(t2)
+            return (max(t2), valveson+1)
 
 # example input line
 # Valve BB has flow rate=13; tunnels lead to valves CC, AA
@@ -40,7 +38,7 @@ def dec16(fname):
         vd[v.group(1)] = [int(v.group(2)), 0, v.group(3).split(', ')]
         if vd[v.group(1)][0] > 0:
             valvecount += 1
-    totflow = bvf(vd, 'AA', 30)
+    (totflow, _) = bvf(vd, 'AA', 30, 0)
     print(f"part 1: {totflow}")
 
     #print(f"part 2: {tfreq}")
