b94124e1deace836388c53c7bd0942f2db03e200
Eric Dennison <ericd@netdenizen.com>
2022-12-16 13:31:07 -0500

    Updated from Brython Server: 12/16/2022 1:31:06 PM

--- dec16.py
+++ dec16.py
@@ -5,9 +5,13 @@ from aocutils import *
 # Sample: 1651 and 
 # Actual: and 
 
+valvecount = 0
+valveson = 0
 #  best valve flow
 def bvf(vd, startvalve, timeleft):
-    print(f"{startvalve} {timeleft}")
+    global valveson
+    if valveson == valvecount:
+        return 0
     if timeleft <= 1:
         # no time left to add more flow
         return 0
@@ -20,6 +24,7 @@ def bvf(vd, startvalve, timeleft):
         if timeleft == 2:
             return v[0]
         else:
+            valveson += 1
             v[1] = 1
             t2 = [v[0]*(timeleft-1) + bvf(vd, xx, timeleft-2) for xx in v[2]]
             return max(t2)
@@ -27,11 +32,14 @@ def bvf(vd, startvalve, timeleft):
 # example input line
 # Valve BB has flow rate=13; tunnels lead to valves CC, AA
 def dec16(fname):
+    global valvecount
     vd = {}
     valves = compile("Valve (\S+) has flow rate=(\d+); tunnels? leads? to valves? (.+)")
     # flow rate, open/closed, connected valve list
     for v in [valves.search(s) for s in flistofstrings(fname)]:
         vd[v.group(1)] = [int(v.group(2)), 0, v.group(3).split(', ')]
+        if vd[v.group(1)][0] > 0:
+            valvecount += 1
     totflow = bvf(vd, 'AA', 10)
     print(f"part 1: {totflow}")
 
