2f8190523e806512562093168ce73257be19b174
Eric Dennison <ericd@netdenizen.com>
2022-12-16 13:52:34 -0500

    Updated from Brython Server: 12/16/2022 1:52:33 PM

--- dec16.py
+++ dec16.py
@@ -7,24 +7,24 @@ from aocutils import *
 
 valvecount = 0
 #  best valve flow
-def bvf(vd, startvalve, timeleft, valveson):
+def bvf(vd, startvalve, timeleft, valveson, totalflow):
     if valveson == valvecount:
-        return (0, valveson)
+        return totalflow
     if timeleft <= 1:
         # no time left to add more flow
-        return (0, valveson)
+        return totalflow
     v = vd[startvalve]
     if v[1] or v[0] == 0:    # valve already open, or 0 flow, get times to other nodes
-        t1 = [bvf(vd, xx, timeleft-1, valveson) for xx in v[2]]
-        return (max(t1), valveson)        
+        t1 = [bvf(vd, xx, timeleft-1, valveson, totalflow) for xx in v[2]]
+        return max(t1)       
     else:
         # assume opening valve: remaining flow for this valve plus the rest
         if timeleft == 2:
-            return (v[0], valveson)
+            return v[0]+totalflow
         else:
             v[1] = 1
-            t2 = [v[0]*(timeleft-1) + bvf(vd, xx, timeleft-2) for xx in v[2]]
-            return (max(t2), valveson+1)
+            t2 = [bvf(vd, xx, timeleft-2, valveson+1, totalflow+v[0]*(timeleft-1)) for xx in v[2]]
+            return max(t2)
 
 # example input line
 # Valve BB has flow rate=13; tunnels lead to valves CC, AA
@@ -37,7 +37,7 @@ def dec16(fname):
         vd[v.group(1)] = [int(v.group(2)), 0, v.group(3).split(', ')]
         if vd[v.group(1)][0] > 0:
             valvecount += 1
-    (totflow, _) = bvf(vd, 'AA', 30, 0)
+    (totflow, _) = bvf(vd, 'AA', 30, 0, 0)
     print(f"part 1: {totflow}")
 
     #print(f"part 2: {tfreq}")
