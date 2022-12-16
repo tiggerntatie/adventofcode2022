5533da3e0fd2f4ce1681ce5f8f6b8c02a2448e7a
Eric Dennison <ericd@netdenizen.com>
2022-12-16 12:36:58 -0500

    Updated from Brython Server: 12/16/2022 12:36:58 PM

--- dec16.py
+++ dec16.py
@@ -6,12 +6,16 @@ from aocutils import *
 # Actual: and 
 
      
-
+# example input line
+# Valve BB has flow rate=13; tunnels lead to valves CC, AA
 def dec16(fname):
-    ins = compile(".*=([\d\-]+).*=([\d\-]+).*=([\d\-]+).*=([\d\-]+)")
-    ml = [[int(m.group(i)) for i in range(1,5)] for m in [ins.search(s) for s in flistofstrings(fname)]]
-    ml = [l + [mdist(*l)] for l in ml]
-    print(f"part 1: {count}")
+    vd = {}
+    valves = compile("Valve () has flow rate=([\d]+]); tunnels? lead to valves? (.+)")
+    #ml = [[int(m.group(i)) for i in range(1,5)] for m in [valvessearch(s) for s in flistofstrings(fname)]]
+    for v in [valves.search(s) for s in flistofstrings(fname)]]
+        vd[v.group(1)] = [int(v.group(2)), v.group(3).split(',')]
+    print(vd)
+    #print(f"part 1: {count}")
 
     #print(f"part 2: {tfreq}")
 
