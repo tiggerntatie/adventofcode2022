74cc6f6a7f1685ee40609cfbc5da2105c55c2851
Eric Dennison <ericd@netdenizen.com>
2022-12-23 13:17:52 -0500

    Create dec18.py

---
+++ dec18.py
@@ -0,0 +1,17 @@
+# Advent of code 18
+from aocutils import *
+from re import compile
+# Sample:   and 
+# Actual:   and 
+
+def dec18(fname):
+    fs = compile("([UDLR]) (\d+)")
+    for move in flistofstrings(fname):
+
+    print(f"part 1: {len(sides)}")
+    print(f"part 2: {len(sides)}")
+ 
+print("Sample")
+dec18("dec18s.txt")
+print("Actual")
+dec18("dec18.txt")
