f5c008fe54d7cc7830d3165ef247b03297f26bb7
Eric Dennison <ericd@netdenizen.com>
2022-12-23 13:24:05 -0500

    Updated from Brython Server: 12/23/2022 1:24:04 PM

--- dec18.py
+++ dec18.py
@@ -1,17 +1,17 @@
 # Advent of code 18
 from aocutils import *
 from re import compile
-# Sample:   and 
+# Sample:  64 and 
 # Actual:   and 
 
 def dec18(fname):
-    fs = compile("([UDLR]) (\d+)")
-    for move in flistofstrings(fname):
-
-    print(f"part 1: {len(sides)}")
-    print(f"part 2: {len(sides)}")
+    cubes = compile("(\d+),(\d+),(\d+)")
+    cl = [x.group(1), x.group(2), x.group(3) for x in [cubes.search(x) for x in flistofstrings(fname)]]
+    print(cl)
+#    print(f"part 1: {len(sides)}")
+#    print(f"part 2: {len(sides)}")
  
 print("Sample")
 dec18("dec18s.txt")
-print("Actual")
-dec18("dec18.txt")
+#print("Actual")
+#dec18("dec18.txt")
