1f7eed7c59ae72a3427aebb4ae420eafd73034fa
Eric Dennison <ericd@netdenizen.com>
2022-12-23 13:47:28 -0500

    Updated from Brython Server: 12/23/2022 1:47:27 PM

--- dec18.py
+++ dec18.py
@@ -1,16 +1,26 @@
 # Advent of code 18
 from aocutils import *
+from collections import Counter
 from re import compile
 # Sample:  64 and 
 # Actual:   and 
 
 def sides(c):
-    return ((c[0],c[1],c[2]+0.5))
+    return set(((c[0],c[1],c[2]+0.5), 
+        (c[0],c[1],c[2]-0.5),
+        (c[0],c[1]+0.5,c[2]),
+        (c[0],c[1]-0.5,c[2]),
+        (c[0]+0.5,c[1],c[2]),
+        (c[0]-0.5,c[1],c[2])))
 
 def dec18(fname):
+    faces = Counter()
     cubes = compile("(\d+),(\d+),(\d+)")
     cl = [(int(x.group(1)), int(x.group(2)), int(x.group(3))) for x in [cubes.search(x) for x in flistofstrings(fname)]]
-    print(cl)
+    for c in cl:
+        faces.update(sides(c))
+    nakedfaces = filter(lambda x: x[1] == 1, faces)
+    print(nakedfaces)
 #    print(f"part 1: {len(sides)}")
 #    print(f"part 2: {len(sides)}")
  
